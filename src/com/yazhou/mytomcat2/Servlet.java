a4e10d0e3c8a5ba0a26a70f910e6d0ec96b4438d
yazhou <1014465379@qq.com>
2021-09-06 10:42:32 +0800

    无nio版本

--- src/com/yazhou/mytomcat2/Servlet.java
+++ src/com/yazhou/mytomcat2/Servlet.java
@@ -3,8 +3,6 @@ package com.yazhou.mytomcat2;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.ByteBuffer;
-
 //所有服务端的java小城内需要实现的接口
 public interface Servlet {
     //初始化
@@ -15,5 +13,4 @@ public interface Servlet {
 
     //销毁
     public void destroy();
-
 }
