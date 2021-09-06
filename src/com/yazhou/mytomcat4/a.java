6bfdd390783b0ff54cb0069ca8da40c7771d60e2
1014465379 <80385820+1014465379@users.noreply.github.com>
2021-09-06 10:46:00 +0800

    Delete src/com/yazhou/mytomcat4 directory

--- src/com/yazhou/mytomcat4/a.java
+++
@@ -1,8 +0,0 @@
-package com.yazhou.mytomcat4;
-
-public class a {
-    public static void main(String[] args) {
-        HttpServer httpServer = new HttpServer();
-        httpServer.acceptWait();
-    }
-}
